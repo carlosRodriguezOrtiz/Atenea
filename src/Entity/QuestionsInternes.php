9fd98459a7d3b8104e304a75dbe68d323d925384
ivanGARCIACAMBLOR <camblorivan@gmail.com>
2019-04-29 20:28:55 +0200

    changes ivan jorge

--- src/Entity/QuestionsInternes.php
+++ src/Entity/QuestionsInternes.php
@@ -14,7 +14,7 @@ class QuestionsInternes
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
-    private $id;
+    public $id;
 
     /**
      * @ORM\Column(type="string", length=255)
