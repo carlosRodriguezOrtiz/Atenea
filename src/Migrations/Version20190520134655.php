e875e7f54c056f2744bbec211347bab0427d76af
AdriaCortesAlvarez <adria_cortesalvarez@iescarlesvallbona.cat>
2019-05-20 17:06:42 +0200

    edit qe && edit qi

--- src/Migrations/Version20190520134655.php
+++ src/Migrations/Version20190520134655.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190519202340 extends AbstractMigration
+final class Version20190520134655 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,13 +22,9 @@ final class Version20190519202340 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE questions_externes ADD empresa_id INT DEFAULT NULL, ADD centro_id INT DEFAULT NULL, ADD corporacion_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE questions_externes ADD CONSTRAINT FK_C3428BEC521E1991 FOREIGN KEY (empresa_id) REFERENCES empresa (id)');
-        $this->addSql('ALTER TABLE questions_externes ADD CONSTRAINT FK_C3428BEC298137A7 FOREIGN KEY (centro_id) REFERENCES centro (id)');
-        $this->addSql('ALTER TABLE questions_externes ADD CONSTRAINT FK_C3428BEC1011F129 FOREIGN KEY (corporacion_id) REFERENCES corporacion (id)');
-        $this->addSql('CREATE INDEX IDX_C3428BEC521E1991 ON questions_externes (empresa_id)');
-        $this->addSql('CREATE INDEX IDX_C3428BEC298137A7 ON questions_externes (centro_id)');
-        $this->addSql('CREATE INDEX IDX_C3428BEC1011F129 ON questions_externes (corporacion_id)');
+        $this->addSql('CREATE TABLE aspecte_q (id INT AUTO_INCREMENT NOT NULL, questions_externes_id INT DEFAULT NULL, questions_internes_id INT DEFAULT NULL, nom VARCHAR(255) NOT NULL, dafo VARCHAR(255) NOT NULL, alta DATETIME NOT NULL, baixa DATETIME NOT NULL, descripcio VARCHAR(255) NOT NULL, INDEX IDX_556D6A54861C8D0B (questions_externes_id), INDEX IDX_556D6A54A6B6EE63 (questions_internes_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE aspecte_q ADD CONSTRAINT FK_556D6A54861C8D0B FOREIGN KEY (questions_externes_id) REFERENCES questions_externes (id)');
+        $this->addSql('ALTER TABLE aspecte_q ADD CONSTRAINT FK_556D6A54A6B6EE63 FOREIGN KEY (questions_internes_id) REFERENCES questions_internes (id)');
         $this->addSql('ALTER TABLE questions_internes ADD empresa_id INT DEFAULT NULL, ADD centro_id INT DEFAULT NULL, ADD corporacion_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE questions_internes ADD CONSTRAINT FK_90E33757521E1991 FOREIGN KEY (empresa_id) REFERENCES empresa (id)');
         $this->addSql('ALTER TABLE questions_internes ADD CONSTRAINT FK_90E33757298137A7 FOREIGN KEY (centro_id) REFERENCES centro (id)');
@@ -43,13 +39,7 @@ final class Version20190519202340 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE questions_externes DROP FOREIGN KEY FK_C3428BEC521E1991');
-        $this->addSql('ALTER TABLE questions_externes DROP FOREIGN KEY FK_C3428BEC298137A7');
-        $this->addSql('ALTER TABLE questions_externes DROP FOREIGN KEY FK_C3428BEC1011F129');
-        $this->addSql('DROP INDEX IDX_C3428BEC521E1991 ON questions_externes');
-        $this->addSql('DROP INDEX IDX_C3428BEC298137A7 ON questions_externes');
-        $this->addSql('DROP INDEX IDX_C3428BEC1011F129 ON questions_externes');
-        $this->addSql('ALTER TABLE questions_externes DROP empresa_id, DROP centro_id, DROP corporacion_id');
+        $this->addSql('DROP TABLE aspecte_q');
         $this->addSql('ALTER TABLE questions_internes DROP FOREIGN KEY FK_90E33757521E1991');
         $this->addSql('ALTER TABLE questions_internes DROP FOREIGN KEY FK_90E33757298137A7');
         $this->addSql('ALTER TABLE questions_internes DROP FOREIGN KEY FK_90E337571011F129');
