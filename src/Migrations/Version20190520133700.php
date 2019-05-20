735136b12bada61e26f8602354ee0b6cb18a0f76
Marc Gomis <marc_gomisgalera@iescarlesvallbona.cat>
2019-05-20 15:52:44 +0200

    volver

--- src/Migrations/Version20190520133700.php
+++ src/Migrations/Version20190520133700.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190519202340 extends AbstractMigration
+final class Version20190520133700 extends AbstractMigration
 {
     public function getDescription() : string
     {
